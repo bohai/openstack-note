425637c579b8227ae35a485860f77d5c40657eb9
bohai <bohai@huawei.com>
2014-05-22 19:42:57 +0800

    Create exportdb.sh

---
+++ openstack/exportdb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+STACK_TEMPLATE="/etc/template"
+
+mysqldump -u root -padmin cinder    > ${STACK_TEMPLATE}/cinder.sql
+mysqldump -u root -padmin glance    > ${STACK_TEMPLATE}/glance.sql
+mysqldump -u root -padmin heat      > ${STACK_TEMPLATE}/heat.sql
+mysqldump -u root -padmin keystone  > ${STACK_TEMPLATE}/keystone.sql
+mysqldump -u root -padmin neutron   > ${STACK_TEMPLATE}/neutron.sql
+mysqldump -u root -padmin nova      > ${STACK_TEMPLATE}/nova.sql
+mysqldump -u root -padmin mysql      > ${STACK_TEMPLATE}/mysql.sql
